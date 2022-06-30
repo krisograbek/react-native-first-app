336f545b6d78567d96a25b981a9266ad3236d729
krisograbek <krzysztof.ograbek@gmail.com>
2022-06-30 10:50:08 +0200

    add text input and button, create state for goals list, add scrollView that still needs fixing

--- App.js
+++ App.js
@@ -1,33 +1,67 @@
-import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import { useState } from 'react';
+import { Button, ScrollView, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+
+  const [newGoal, setNewGoal] = useState("")
+  const [goalsList, setGoalsList] = useState([])
+
+  const handleGoalInput = enteredText => {
+    setNewGoal(enteredText)
+  }
+
+  const handleAddGoal = () => {
+    // using a callback with the previous state is the recommended way of updating state
+    // when it depends on the previous state
+    setGoalsList(prevState => [...prevState, newGoal])
+  }
+
   return (
     <View style={styles.appContainer}>
       <View style={styles.inputContainer}>
-        <TextInput style={styles.textInput} placeholder="Your goal" />
-        <Button title="Add Goal" />
+        <TextInput style={styles.textInput} placeholder="Your goal" onChangeText={handleGoalInput} />
+        <Button title="Add Goal" onPress={handleAddGoal} />
       </View>
-      <View>
-        <Text>List of goals</Text>
-      </View>
+      <ScrollView style={styles.goalsContainer}>
+        {/* <Text>List of goals</Text> */}
+        {goalsList.map((goal, i) => (
+          <Text key={i} style={styles.goalItem}>{goal}</Text>
+        ))}
+      </ScrollView>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   appContainer: {
+    flex: 1,    // to take 100% of device height
     paddingTop: 80,
+    paddingHorizontal: 30
   },
   inputContainer: {
+    flex: 1,
     flexDirection: 'row',
     justifyContent: 'space-between',
-    paddingHorizontal: 30
+    alignItems: 'center',
+    marginBottom: 32,
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc'
   },
   textInput: {
     borderWidth: 1,
     borderColor: '#cccccc',
     width: '70%',
     marginRight: 8,
-    paddingLeft: 8
+    padding: 8
+  },
+  goalsContainer: {
+    flex: 4
+  },
+  goalItem: {
+    fontSize: 24,
+    marginVertical: 8,
+    padding: 8,
+    borderRadius: 6,
+    backgroundColor: '#dd74e8'
   }
 });
